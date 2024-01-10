74fb5a807088827d35a892fe18b1d755452324f3
KomendaKacper <komenda@student.agh.edu.pl>
2024-01-10 23:54:59 +0100

    Lecisz z tym wariacie!

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
 path("<str:name>", views.index, name="index"),
-path("", views.home, name = "home")
+path("", views.home, name = "home"),
+path("create/", views.create, name = "create"),
 ]

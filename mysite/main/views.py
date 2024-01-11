617a0dc2fbc4fe8796350074c346a0f0ec966f8d
KomendaKacper <komenda@student.agh.edu.pl>
2024-01-11 21:35:06 +0100

    jh

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -6,11 +6,40 @@ from .forms import CreateNewList
 
 def index(response, name):
     ls = ToDoList.objects.get(name = name)
+    if response.method == "POST":
+        print (response.POST)
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+                
+                item.save()
+        
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+
+            if len(txt) > 2:
+                ls.item_ser.create(text=txt, complete=False)
+            else:
+                print ("invalid")
+
+
+
     return render (response, "main/list.html", {"ls":ls})
 
 def home(response):
     return render (response, "main/home.html", {})
 
 def create(response):
-    form = CreateNewList()
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name=n)
+            t.save()
+    else:
+        form = CreateNewList()
     return render(response, "main/create.html", {"form":form})
